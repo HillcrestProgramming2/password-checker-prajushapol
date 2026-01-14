fb24dbfd78a672481e54291bafe1552788b839ea
prajushapol <prajushapol@gmail.com>
2026-01-13 22:33:34 -0700

    Update CriteriaCheckerTest.java

--- Password Checker App/src/test/java/org/hillcrest/chapter6/password/CriteriaCheckerTest.java
+++ Password Checker App/src/test/java/org/hillcrest/chapter6/password/CriteriaCheckerTest.java
@@ -1,7 +1,28 @@
 package org.hillcrest.chapter6.password;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CriteriaCheckerTest {
 
+    @Test
+    public void testWeakPassword() {
+        assertEquals(1, CriteriaChecker.evaluateCriteria("12345"));
+        assertEquals("Weak", CriteriaChecker.determineStrength(1));
+    }
+
+    @Test
+    public void testModeratePassword() {
+        assertEquals(3, CriteriaChecker.evaluateCriteria("password123"));
+        assertEquals("Moderate", CriteriaChecker.determineStrength(3));
+    }
+
+    @Test
+    public void testStrongPassword() {
+        assertEquals(5, CriteriaChecker.evaluateCriteria("Secure@2024"));
+        assertEquals("Strong", CriteriaChecker.determineStrength(5));
+    }
 }
+
